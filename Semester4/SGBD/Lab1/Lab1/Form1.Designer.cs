2e685835133fe9a07caf2e567175a3c3bef6e20a
Titieni Paul <154063842+tpaul790@users.noreply.github.com>
2025-03-17 14:48:33 +0200

    lab1

---
+++ Semester4/SGBD/Lab1/Lab1/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 109);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(792, 431);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(32, 595);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 82;
+            this.dataGridView2.RowTemplate.Height = 33;
+            this.dataGridView2.Size = new System.Drawing.Size(792, 431);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 62);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(299, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Parent Table";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(299, 567);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Child Table";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1084, 371);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(331, 31);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(981, 542);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Fuel";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(886, 484);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Boxes Capacity";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(966, 429);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Brand";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(840, 377);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(208, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Registration Number";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1084, 423);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(331, 31);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1084, 478);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(331, 31);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1084, 536);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(331, 31);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(1084, 594);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(331, 31);
+            this.textBox5.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(972, 600);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 25);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Miles";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1267, 660);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 62);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "UPDATE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1113, 660);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 62);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "REMOVE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(959, 660);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(148, 62);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "ADD";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1470, 1038);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Laborator1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Semester4/SGBD/Lab1/Lab1/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+namespace Lab1
+{
+    public partial class Form1: Form
+    {
+        private SqlConnection sqlConnection = new SqlConnection("Server=192.168.1.20;Database=CompanieCurierat;User Id=sa;Password=MyStrongPass123;TrustServerCertificate=true");
+        private SqlDataAdapter dataAdapter = new SqlDataAdapter();
+        private DataSet dataSetParent = new DataSet(); 
+        private DataSet dataSetChild = new DataSet();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(sqlConnection.State != ConnectionState.Open)
+                sqlConnection.Open();
+            dataAdapter.SelectCommand = new SqlCommand("SELECT * FROM Soferi",sqlConnection);
+            dataSetParent.Clear();
+            dataAdapter.Fill(dataSetParent);
+            dataGridView1.DataSource = dataSetParent.Tables[0];
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (dataGridView2.SelectedRows.Count > 0)
+            {
+                if (textBox2.TextLength != 0 && textBox3.TextLength != 0 && textBox4.TextLength != 0 && textBox5.TextLength != 0)
+                {
+                    string registrationNumber = (string)dataGridView2.SelectedRows[0].Cells[0].Value;
+                    string brand = textBox2.Text;
+                    int boxesCapacity = int.Parse(textBox3.Text);
+                    string fuel = textBox4.Text;
+                    int mileage = int.Parse(textBox5.Text);
+
+                    SqlCommand command = new SqlCommand("UPDATE Masini SET marca = @brand, capacitateCuti = @boxesCapacity, combustibil = @fuel, kilometraj = @mileage WHERE nrInmatriculare = @registrationNr", sqlConnection);
+                    command.Parameters.AddWithValue("@brand", brand);
+                    command.Parameters.AddWithValue("@boxesCapacity", boxesCapacity);
+                    command.Parameters.AddWithValue("@fuel", fuel);
+                    command.Parameters.AddWithValue("@mileage", mileage);
+                    command.Parameters.AddWithValue("@registrationNr", registrationNumber);
+
+                    dataAdapter.UpdateCommand = command;
+                    dataAdapter.UpdateCommand.ExecuteNonQuery();
+                    dataSetChild.Clear();
+                    dataAdapter.Fill(dataSetChild);
+                    dataGridView2.DataSource = dataSetChild.Tables[0];
+
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    textBox3.Clear();
+                    textBox4.Clear();
+                    textBox5.Clear();
+                    MessageBox.Show("The car with registration plate: " + registrationNumber + " succesfully updated!");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView2.SelectedRows.Count > 0)
+            {
+                string registrationNr = (string)dataGridView2.SelectedRows[0].Cells[0].Value;
+                SqlCommand command = new SqlCommand("DELETE FROM Masini WHERE nrInmatriculare = @rp", sqlConnection);
+                command.Parameters.AddWithValue("@rp", registrationNr);
+
+                dataAdapter.DeleteCommand = command;
+                dataAdapter.DeleteCommand.ExecuteNonQuery();
+                dataSetChild.Clear();
+                dataAdapter.Fill(dataSetChild);
+                dataGridView2.DataSource = dataSetChild.Tables[0];
+                MessageBox.Show("The car with registration plate: " + registrationNr + " succesfully deleted");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(dataGridView1.SelectedRows.Count > 0)
+            {
+                if (textBox1.TextLength != 0 && textBox2.TextLength != 0 && textBox3.TextLength != 0 && textBox4.TextLength != 0 && textBox5.TextLength != 0)
+                {
+                    string registrationNumber = textBox1.Text;
+                    string brand = textBox2.Text;
+                    int boxesCapacity = int.Parse(textBox3.Text);
+                    string fuel = textBox4.Text;
+                    int mileage = int.Parse(textBox5.Text);
+                    int idSofer = (int)dataGridView1.SelectedRows[0].Cells[0].Value;
+                    SqlCommand command = new SqlCommand("INSERT INTO Masini Values(@rp,@brand,@capacity,@fuel,@mileage,@driver)", sqlConnection);
+                    command.Parameters.AddWithValue("@rp", registrationNumber);
+                    command.Parameters.AddWithValue("@brand", brand);
+                    command.Parameters.AddWithValue("@capacity", boxesCapacity);
+                    command.Parameters.AddWithValue("@fuel", fuel);
+                    command.Parameters.AddWithValue("@mileage", mileage);
+                    command.Parameters.AddWithValue("@driver", idSofer);
+
+                    dataAdapter.InsertCommand = command;
+                    dataAdapter.InsertCommand.ExecuteNonQuery();
+                    dataSetChild.Clear();
+                    dataAdapter.Fill(dataSetChild);
+                    dataGridView2.DataSource = dataSetChild.Tables[0];
+
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    textBox3.Clear();
+                    textBox4.Clear();
+                    textBox5.Clear();
+
+                    MessageBox.Show("New car succesfully added");
+                }
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                if (sqlConnection.State != ConnectionState.Open)
+                    sqlConnection.Open();
+                DataGridViewRow row = dataGridView1.SelectedRows[0];
+                int idSofer = (int)row.Cells[0].Value;
+                SqlCommand selectCommand = new SqlCommand("SELECT * FROM Masini WHERE idSofer = @idS", sqlConnection);
+                selectCommand.Parameters.AddWithValue("@idS", idSofer);
+                dataAdapter.SelectCommand = selectCommand;
+                dataSetChild.Clear();
+                dataAdapter.Fill(dataSetChild);
+                dataGridView2.DataSource = dataSetChild.Tables[0];
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
